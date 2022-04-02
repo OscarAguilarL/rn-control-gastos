d9372836afcc026e6783a7d35a53f54972d7b42e
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-04-01 18:13:07 -0600

    Mostrando imagenes de la categoria de cada gasto

--- src/components/Gasto.js
+++ src/components/Gasto.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import {StyleSheet, Text, View, Image, Pressable} from 'react-native';
+import {formatCurrency} from '../helpers';
 
 import globalStyles from '../styles';
+
+const iconos = {
+  ahorro: require('../img/icono_ahorro.png'),
+  comida: require('../img/icono_comida.png'),
+  casa: require('../img/icono_casa.png'),
+  salud: require('../img/icono_salud.png'),
+  ocio: require('../img/icono_ocio.png'),
+  suscripciones: require('../img/icono_suscripciones.png'),
+  gastos: require('../img/icono_gastos.png'),
+};
 
 const Gasto = ({gasto}) => {
   const {nombre, categoria, cantidad, id} = gasto;
 
   return (
     <View style={styles.contenedor}>
-      <Text>{nombre}</Text>
+      <View>
+        <Image source={iconos[categoria]} />
+
+        <View>
+          <Text>{categoria}</Text>
+          <Text>{nombre}</Text>
+        </View>
+
+        <Text>{formatCurrency(cantidad)}</Text>
+      </View>
     </View>
   );
 };
