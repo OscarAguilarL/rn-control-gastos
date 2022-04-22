7e85edac60b1156c7314a99054ce7c1d12a78c4a
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-04-22 16:38:36 -0500

    Mostrando Gastos Filtrados

--- src/components/ListadoGastos.js
+++ src/components/ListadoGastos.js
@@ -2,17 +2,37 @@ import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import Gasto from './Gasto';
 
-const ListadoGastos = ({gastos = [], setModal, setGasto}) => {
+const ListadoGastos = ({
+  gastos = [],
+  setModal,
+  setGasto,
+  filtro,
+  gastosFiltrados,
+}) => {
   return (
     <View style={styles.contenedor}>
       <Text style={styles.titulo}>Listado de Gastos</Text>
 
-      {gastos.length === 0 ? (
+      {filtro
+        ? gastosFiltrados.map(gasto => (
+            <Gasto
+              key={gasto.id}
+              gasto={gasto}
+              setModal={setModal}
+              setGasto={setGasto}
+            />
+          ))
+        : gastos.map(gasto => (
+            <Gasto
+              key={gasto.id}
+              gasto={gasto}
+              setModal={setModal}
+              setGasto={setGasto}
+            />
+          ))}
+
+      {(gastos.length === 0 || (gastosFiltrados.length === 0 && !!filtro)) && (
         <Text style={styles.noGastos}>No hay gastos</Text>
-      ) : (
-        gastos.map(gasto => (
-          <Gasto key={gasto.id} gasto={gasto} setModal={setModal} setGasto={setGasto} />
-        ))
       )}
     </View>
   );
